910d570eb83de0faf0270429438aa3a2b79a45ca
Brian Cheung <bcheung.comp@gmail.com>
2018-07-21 11:41:41 -0500

    firebase config gitignore

--- src/FirebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export const FIREBASE_CONFIG = {
-  apiKey: 'AIzaSyAP0dDmyzWRUrg53QwhIgLLpLBHa7_YGas',
-  authDomain: 'manager-comp.firebaseapp.com',
-  databaseURL: 'https://manager-comp.firebaseio.com',
-  projectId: 'manager-comp',
-  storageBucket: '',
-  messagingSenderId: '863357632054'
-};
